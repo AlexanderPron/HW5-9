a1e717615b4f5ca711e7005f90621351685df6af
Gadya <zakuson2001@inbox.ru>
2020-08-18 23:53:15 +0300

    hw B5-9 v 1.0 no Class

--- hw-b5-9.py
+++ hw-b5-9.py
@@ -1,4 +1,23 @@
 import time
+# class Timer():
+#     def __init__(self):
+#         pass
+#     def __enter__(self):
+#         pass
+#     def __exit__(self):
+#         pass
+
+# def fibo(num):
+#     rez = [1,2,]
+#     fibo_num = 2 
+#     while fibo_num < num:
+#         fibo_num = rez[len(rez)-1]+ rez[len(rez)-2]
+#         if fibo_num < num:
+#             rez.append(fibo_num)
+#     time.sleep(1)
+#     return rez
+
+
 def time_this(num_runs):
     def my_decor(func):
         def wrapper(num):
@@ -7,11 +26,11 @@ def time_this(num_runs):
                 t1= time.time()
                 func(num)
                 t2= time.time()
-                delta = t2 - t1
+                delta = t2 - t1 - 1
                 avg_time += delta
-                print('{} проход: {}'.format(i+1,delta -1))
+                print('{} проход: {}'.format(i+1,delta))
             avg_time /= num_runs    
-            return avg_time -1
+            return avg_time
         return wrapper
     return my_decor
 @time_this(num_runs=10)
