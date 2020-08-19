f3f65cf64cf92a9bf3a604473413a088d33a7536
AlexanderPron <alexpronin85@gmail.com>
2020-08-19 13:18:56 +0300

    hw B5-9 v 1.2 final class + context manager

--- hw-b5-9.py
+++ hw-b5-9.py
@@ -3,6 +3,10 @@ from functools import wraps
 class Timer():
     def __init__(self, num_runs):
         self.num_runs = num_runs
+
+    def __enter__(self):
+        self.t0= time.time()
+
     def __call__(self, func):
         @wraps(func) # Нужно для корректного отображения названий декорируемых функций (если не использовать wraps, то func.__name__ всегда будет wrapper)
         def wrapper(*args, **kwargs):
@@ -11,19 +15,23 @@ class Timer():
                 t1= time.time()
                 func(*args, **kwargs)
                 t2= time.time()
-                delta = t2 - t1
-                avg_time += delta
-                print('{} проход: {}'.format(i+1,delta))
+                delta1 = t2 - t1
+                avg_time += delta1
+                print('{} проход: {}'.format(i+1, delta1))
             avg_time /= self.num_runs    
             return avg_time
         return wrapper
 
-    def __enter__(self):
-        pass
-    def __exit__(self):
-        pass
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.t4= time.time()
+        delta2 = self.t4 - self.t0
+        print('{} проходов выполнено за {} секунд'.format(self.num_runs, delta2))
 
-@Timer(num_runs = 10)
+
+num_runs1 = 10
+num_runs2 = 20
+
+@Timer(num_runs1)
 def fibo(num):
     rez = [1,2,]
     fibo_num = 2 
@@ -32,15 +40,19 @@ def fibo(num):
         if fibo_num < num:
             rez.append(fibo_num)
     return rez
-@Timer(num_runs = 20)
+@Timer(num_runs2)
 def f():
     for _ in range(1000000):
         pass
 
 def main():
-    print('Среднее время выполнения функции {} : {}'.format(fibo.__name__, fibo(10**164)))
-    print('Среднее время выполнения функции {} : {}'.format(f.__name__, f()))
+    with Timer(num_runs1):
+        print('Среднее время выполнения функции {} : {}'.format(fibo.__name__, fibo(10**164)))
+    with Timer(num_runs2):
+        print('Среднее время выполнения функции {} : {}'.format(f.__name__, f()))
 
+    # print('Среднее время выполнения функции {} : {}'.format(fibo.__name__, fibo(10**164)))
+    # print('Среднее время выполнения функции {} : {}'.format(f.__name__, f()))
 
 if __name__ == "__main__":
     main()
