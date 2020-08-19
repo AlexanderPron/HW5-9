50a506043c21c23a23924186f329815e619d7828
AlexanderPron <alexpronin85@gmail.com>
2020-08-19 10:44:30 +0300

    hw B5-9 v 1.1 decorate as Class

--- hw-b5-9.py
+++ hw-b5-9.py
@@ -1,39 +1,29 @@
 import time
-# class Timer():
-#     def __init__(self):
-#         pass
-#     def __enter__(self):
-#         pass
-#     def __exit__(self):
-#         pass
-
-# def fibo(num):
-#     rez = [1,2,]
-#     fibo_num = 2 
-#     while fibo_num < num:
-#         fibo_num = rez[len(rez)-1]+ rez[len(rez)-2]
-#         if fibo_num < num:
-#             rez.append(fibo_num)
-#     time.sleep(1)
-#     return rez
-
-
-def time_this(num_runs):
-    def my_decor(func):
-        def wrapper(num):
+from functools import wraps
+class Timer():
+    def __init__(self, num_runs):
+        self.num_runs = num_runs
+    def __call__(self, func):
+        @wraps(func) # Нужно для корректного отображения названий декорируемых функций (если не использовать wraps, то func.__name__ всегда будет wrapper)
+        def wrapper(*args, **kwargs):
             avg_time = 0
-            for i in range(num_runs):
+            for i in range(self.num_runs):
                 t1= time.time()
-                func(num)
+                func(*args, **kwargs)
                 t2= time.time()
-                delta = t2 - t1 - 1
+                delta = t2 - t1
                 avg_time += delta
                 print('{} проход: {}'.format(i+1,delta))
-            avg_time /= num_runs    
+            avg_time /= self.num_runs    
             return avg_time
         return wrapper
-    return my_decor
-@time_this(num_runs=10)
+
+    def __enter__(self):
+        pass
+    def __exit__(self):
+        pass
+
+@Timer(num_runs = 10)
 def fibo(num):
     rez = [1,2,]
     fibo_num = 2 
@@ -41,11 +31,16 @@ def fibo(num):
         fibo_num = rez[len(rez)-1]+ rez[len(rez)-2]
         if fibo_num < num:
             rez.append(fibo_num)
-    time.sleep(1)
     return rez
+@Timer(num_runs = 20)
+def f():
+    for _ in range(1000000):
+        pass
 
 def main():
-    print(fibo(10**164))
+    print('Среднее время выполнения функции {} : {}'.format(fibo.__name__, fibo(10**164)))
+    print('Среднее время выполнения функции {} : {}'.format(f.__name__, f()))
+
 
 if __name__ == "__main__":
     main()
